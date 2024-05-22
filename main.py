b1cb7e38cb7c1ff322fc53ec6ad576a3cce07ccf
TimeSlayer <nikitajudo313@gmail.com>
2024-05-22 15:10:28 +0300

    feat: implement bass boost option

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 MAX_FILE_SIZE = 20 * 1024 * 1024  # 20 MB
 
 
-def opus_encode(file_bytes):
+def opus_encode(file_bytes, bass_boost=False):
     with tempfile.NamedTemporaryFile(delete=False) as fp:
         fp.write(file_bytes)
         input_file = fp.name
@@ -15,8 +15,9 @@ def opus_encode(file_bytes):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.ogg') as fp:
         output_file = fp.name
 
+    gain = 10 if bass_boost else 1
     subprocess.run(
-        ["ffmpeg", "-i", input_file, "-vn", "-filter:a", "volume=1, bass=gain=1",
+        ["ffmpeg", "-i", input_file, "-vn", "-filter:a", f"bass=gain={gain}",
          "-codec:a", "libopus", output_file, '-y'])
 
     with open(output_file, 'rb') as f:
@@ -59,24 +60,28 @@ def main(bot_token):
 
         bot.send_voice(message.chat.id, ogg_file_bytes, reply_to_message_id=message.message_id, reply_markup=markup)
 
-    @bot.callback_query_handler(func=lambda call: True)
-    def handle_callback_query(call):
-        if call.data == "add_caption":
-            request_message = bot.send_message(call.message.chat.id, "Send the caption text",
-                                               reply_to_message_id=call.message.message_id)
-            bot.register_next_step_handler(call.message, process_add_caption, call.message, request_message.message_id)
-        elif call.data == "bass_boost":
-            bot.send_message(call.message.chat.id, "Bass boosting is in development",
-                             reply_to_message_id=call.message.message_id)
+    @bot.callback_query_handler(func=lambda call: call.data == "add_caption")
+    def handle_add_caption_callback(call):
+        request_message = bot.send_message(call.message.chat.id, "Send the caption text",
+                                           reply_to_message_id=call.message.message_id)
+        bot.register_next_step_handler(call.message, process_caption_response, call.message,
+                                       request_message.message_id)
 
-    def process_add_caption(message, original_voice_message, request_message_id):
+    @bot.callback_query_handler(lambda call: call.data == "bass_boost")
+    def handle_bass_boost_callback(call):
+        file_id = call.message.voice.file_id
+        file_info = bot.get_file(file_id)
+        file_bytes = bot.download_file(file_info.file_path)
+        bass_boost_file_bytes = opus_encode(file_bytes, True)
+        bot.send_voice(call.message.chat.id, bass_boost_file_bytes, reply_to_message_id=call.message.message_id)
+
+    def process_caption_response(message, original_voice_message, request_message_id):
         voice_file_info = bot.get_file(original_voice_message.voice.file_id)
         voice_file_bytes = bot.download_file(voice_file_info.file_path)
 
         bot.send_voice(message.chat.id, voice_file_bytes, caption=message.text,
                        reply_to_message_id=original_voice_message.message_id)
-        bot.delete_message(message.chat.id, request_message_id)
-        bot.delete_message(message.chat.id, message.message_id)
+        bot.delete_messages(message.chat.id, [request_message_id, message.message_id])
 
     bot.infinity_polling()
 
